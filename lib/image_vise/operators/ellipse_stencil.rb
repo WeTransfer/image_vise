3a7d859b6c573176d28e879de55e84a307ae3437
Julik Tarkhanov <me@julik.nl>
2016-12-07 23:24:02 +0100

    Make ellipse stencil work with transparencie (#6)
    * Fix ellipse stencil for a png with a transparent background
    * Change a PNG image for a test
    * Add a much more treacherous (revealing) test image
    * Remove the palm tree thingy
    * Formatting
    * Small steps, small refactors
    * And fixed (with correct multiplication)
    * Explain the operation in a bit more detail

--- lib/image_vise/operators/ellipse_stencil.rb
+++ lib/image_vise/operators/ellipse_stencil.rb
@@ -12,9 +12,44 @@ class ImageVise::EllipseStencil
   private_constant :C_black
 
   def apply!(magick_image)
-    # http://stackoverflow.com/a/13329959/153886
     width, height = magick_image.columns, magick_image.rows
     
+    # This is a bit involved. We need to do a manual composite. Here is what it entails.
+    #
+    # Given a premultiplied RGB image B, and a grayscale mask A, we need to do the following
+    # operation:
+    #
+    #    BrBgBb / Ba * (Ba * A)
+    #
+    # Since ImageMagick works with unpremultiplied alphas, it is doable - but special care
+    # must be taken not to overmult or overdivide.
+    #
+    # To begin,generate a black and white image for the stencil
+    circle_img = Magick::Image.new(width, height)
+    draw_circle(circle_img, width, height)
+    mask = circle_img.negate
+    
+    # At this stage the mask contains a B/W image of the circle, black outside, white inside.
+    # Retain the alpha of the original in a separate image
+    only_alpha = magick_image.copy
+    only_alpha.alpha(Magick::ExtractAlphaChannel)
+    mask.composite!(only_alpha, Magick::CenterGravity, Magick::MultiplyCompositeOp)
+    
+    # With this composite op, enabling alpha on the destination image is
+    # not required - it will be enabled automatically.
+    # The CopyOpacityCompositeOp implies that we copy the grayscale version
+    # of the RGB channels as the alpha channel, so for some weird reason we need
+    # to disable the alpha on our mask image
+    mask.alpha(Magick::DeactivateAlphaChannel)
+    # And perform the operation (set gray(RGB) of mask as the A of magick_image)
+    magick_image.composite!(mask, Magick::CenterGravity, Magick::CopyOpacityCompositeOp)
+  ensure
+    [mask, only_alpha, circle_img].each do |maybe_image|
+      ImageVise.destroy(maybe_image)
+    end
+  end
+  
+  def draw_circle(into_image, width, height)
     center_x = (width / 2.0)
     center_y = (height / 2.0)
     # Make sure all the edges are anti-aliased
@@ -24,19 +59,9 @@ class ImageVise::EllipseStencil
     gc = Magick::Draw.new
     gc.fill C_black
     gc.ellipse(center_x, center_y, radius_width, radius_height, deg_start=0, deg_end=360)
-    
-    circle_img = Magick::Image.new(width, height)
-    gc.draw(circle_img)
-    
-    mask = circle_img.negate
-    mask.matte = false
-    
-    magick_image.matte = true
-    magick_image.composite!(mask, Magick::CenterGravity, Magick::CopyOpacityCompositeOp)
+    gc.draw(into_image)
   ensure
-    [mask, gc, circle_img].each do |maybe_image|
-      ImageVise.destroy(maybe_image)
-    end
+    ImageVise.destroy(gc)
   end
   
   ImageVise.add_operator 'ellipse_stencil', self
