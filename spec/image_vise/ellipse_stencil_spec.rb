a4545e4db3152919c1d71f281431ea3771ac7db5
Julik Tarkhanov <me@julik.nl>
2016-12-07 21:46:55 +0100

    Formatting

--- spec/image_vise/ellipse_stencil_spec.rb
+++ spec/image_vise/ellipse_stencil_spec.rb
@@ -7,8 +7,9 @@ describe ImageVise::EllipseStencil do
     stencil.apply!(image)
     examine_image(image, "circle-stencil")
   end
+
   it 'applies the circle stencil to a png with transparency' do
-    png_transparent_path = File.expand_path(__dir__ + '/../palm_fronds.png')
+    png_transparent_path = File.expand_path(__dir__ + '/../waterside_magic_hour_transp.png')
     image = Magick::Image.read(png_transparent_path)[0]
     stencil = described_class.new
     stencil.apply!(image)
