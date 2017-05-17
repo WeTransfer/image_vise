d7e00ba4874ed74ada98c438494ae61066adbaf2
Noah Berman <noah@wetransfer.com>
2017-05-17 15:27:10 +0200

    cleans up initial stress test tests

--- spec/image_vise/stress_test_spec.rb
+++ spec/image_vise/stress_test_spec.rb
@@ -18,15 +18,34 @@ describe ImageVise::RenderEngine do
       ImageVise.reset_secret_keys!
     end
 
+    it 'processes a 1.5mb psd' do
+      uri = Addressable::URI.parse(public_url_psd)
+      ImageVise.add_allowed_host!(uri.host)
+      ImageVise.add_secret_key!('1337ness')
+
+      p = ImageVise::Pipeline.new.geom(geometry_string: 'x220')
+      image_request = ImageVise::ImageRequest.new(src_url: uri.to_s, pipeline: p)
+
+      get image_request.to_path_params('1337ness')
+      examine_image_from_string(last_response.body)
+      expect(last_response.headers['Content-Type']).to eq('image/png')
+      expect(last_response.headers['Content-Length']).to eq("94323")
+      expect(last_response.status).to eq(200)
+    end
+
+    # Have to find a better way of running this test w/large files
     it 'processes a 53mb psd' do
       uri = Addressable::URI.parse(public_url_large_psd)
       ImageVise.add_allowed_host!(uri.host)
-      ImageVise.add_secret_key!('showmewhatyougot')
+      ImageVise.add_secret_key!('w00t')
 
-      p = ImageVise::Pipeline.new.geom(geometry_string: '1920x1080')
+      p = ImageVise::Pipeline.new.geom(geometry_string: 'x2000')
       image_request = ImageVise::ImageRequest.new(src_url: uri.to_s, pipeline: p)
 
-      get image_request.to_path_params('showmewhatyougot')
+      get image_request.to_path_params('w00t')
+      examine_image_from_string(last_response.body)
+      expect(last_response.headers['Content-Type']).to eq('image/png')
+      expect(last_response.headers['Content-Length']).to eq("567321")
       expect(last_response.status).to eq(200)
     end
 
