bfc7858b7ce461e6674e1109e24e24c4fc4043d7
Julik Tarkhanov <me@julik.nl>
2017-06-16 02:36:20 +0200

    Switch to the boring composite backfill method

--- lib/image_vise/operators/background_fill.rb
+++ lib/image_vise/operators/background_fill.rb
@@ -10,8 +10,18 @@ class ImageVise::BackgroundFill < Ks.strict(:color)
   end
 
   def apply!(image)
-    image.border!(0, 0, color)
+    # Create an image filled with our color, preserving the size, color class and dimensions
+    fill_image = image.copy
+    fill_image.color_reset!(color)
+
+    # Composite our actual image _on top_ of it, using the standard over composite operator.
+    # This way we don't have to look for "UnderCompositeOp" within the bowels of RMagick.
+    fill_image.composite!(image, x_off=0, y_off=0, Magick::OverCompositeOp)
+    # ..and move the resulting pixels into our original images, replacing everything
+    image.composite!(fill_image, x_off=0, y_off=0, Magick::CopyCompositeOp)
     image.alpha(Magick::DeactivateAlphaChannel)
+  ensure
+    ImageVise.destroy(fill_image)
   end
 
   ImageVise.add_operator 'background_fill', self
